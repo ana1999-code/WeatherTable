982d00734e7aac51f6ec13eae410e0ed6de56a3c
Anisoara <anavlas1999@gmail.com>
2025-08-31 13:58:41 +0300

    Add Weather model

---
+++ src/main/java/org/example/model/WeatherForecast.java
@@ -0,0 +1,58 @@
+package org.example.model;
+
+public class WeatherForecast {
+    private String cityName;
+    private Double minTemp;
+    private Double maxTemp;
+    private Double humidity;
+    private Double windSpeed;
+    private Double windDirection;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public Double getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(Double minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public Double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(Double maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
+
+    public Double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(Double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public Double getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(Double windDirection) {
+        this.windDirection = windDirection;
+    }
+}
