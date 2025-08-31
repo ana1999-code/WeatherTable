fee7c90aed794d20e585b419958a848df6d566e5
Anisoara <anavlas1999@gmail.com>
2025-08-31 19:11:00 +0300

    Calculate column size

--- src/main/java/org/example/printer/TablePrinter.java
+++ src/main/java/org/example/printer/TablePrinter.java
@@ -17,31 +17,47 @@ public class TablePrinter<K, V> implements Printer<Map<K, List<V>>> {
             System.out.println("No data to print.");
             return;
         }
+        int columnSize = getColumnSize(content);
 
         System.out.println("Table content");
-        printLineSeparator(false);
-        columns.forEach(column -> System.out.printf("|%-" + getColumnSize() + "s", column));
+        printLineSeparator(columnSize, false);
+        columns
+                .forEach(
+                        column -> System.out.printf("|%-" + getColumnSize(content) + "s", column)
+                );
 
-        printLineSeparator(true);
+        printLineSeparator(columnSize, true);
         content.forEach((key, values) -> {
-            System.out.printf("|%-" + getColumnSize() + "s", key);
-            values.forEach(value -> System.out.printf("|%-" + getColumnSize() + "s", value));
-            printLineSeparator(true);
+            System.out.printf("|%-" + columnSize + "s", key);
+            values.forEach(value -> System.out.printf("|%-" + columnSize + "s", value));
+            printLineSeparator(columnSize, true);
         });
     }
 
-    private int getColumnSize() {
-        return columns.stream()
+    private int getColumnSize(Map<K, List<V>> content) {
+        int maxColumnSize = columns.stream()
                 .mapToInt(String::length)
                 .max()
                 .orElse(0);
+
+        int maxKeySize = content.keySet().stream().map(Object::toString)
+                .mapToInt(String::length)
+                .max()
+                .orElse(0);
+
+        int maxValueSize = content.values().stream().flatMap(List::stream).map(Object::toString)
+                .mapToInt(String::length)
+                .max()
+                .orElse(0);
+
+        return Math.max(maxColumnSize, Math.max(maxKeySize, maxValueSize));
     }
 
-    private void printLineSeparator(boolean printPipe) {
+    private void printLineSeparator(int columnSize, boolean printPipe) {
         if (printPipe) {
             System.out.println("|");
         }
-        for (int i = 0; i < getColumnSize() * columns.size() + columns.size() + 1; i++) {
+        for (int i = 0; i < columnSize * columns.size() + columns.size() + 1; i++) {
             System.out.print("-");
         }
         System.out.println();
